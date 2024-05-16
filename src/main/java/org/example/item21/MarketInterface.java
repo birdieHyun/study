ff2dc3a75b7871772c24dc2192da7afc18ec6e7e
birdie <flaehdan@gmail.com>
2024-05-16 16:24:06 +0900

    item 21

---
+++ src/main/java/org/example/item21/MarketInterface.java
@@ -0,0 +1,8 @@
+package org.example.item21;
+
+public interface MarketInterface {
+
+    default void hello() {
+        System.out.println("hello interface");
+    }
+}
---
+++ src/main/java/org/example/item21/SuperClass.java
@@ -0,0 +1,9 @@
+package org.example.item21;
+
+public class SuperClass {
+
+    private void hello() {
+        System.out.println("hello class");
+    }
+
+}
